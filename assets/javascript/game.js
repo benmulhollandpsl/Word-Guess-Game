b6719972e7b94e6f0a4c46f664d05231d8614d36
Ben Mulholland <ben.mulholland@gmail.com>
2020-10-02 07:22:20 -0400

    cleaned up most comments, left in comment about testing wins, added footer, need to clarify git link and col for bottom, will check responsiveness then make further changes, removed moot audio function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,5 @@
 $(document).ready(function() {
-// half psuedocode half plan of attack
-// 1. Picking a 'random' word. Maybe not so random considering there is a themed background
-// 2. Take the player’s guess.
-// 3. Quit the game if the player wants to... working on a time out or reset button maybe
-// 4. Check that the player’s guess is a valid letter.
-// 5. Keep track of letters the player has guessed.
-// 6. Show the player their progress.
-// 7. Finish when the player has guessed the word. add a point to their score either way
-// 8. try not to plagerize another persons work, study extra hard js I don't understand
-// 9. notes on adjustments to scope at current build.
-// 9a. want to have a song play with associated words, and stop soon (so maybe just a 15 second clip of each robo.ch)
 
-// instructions
-// 1. Play a sound or song when the user guesses their word correctly, like in our demo.
-// 2. Write some stylish CSS rules to make a design that fits your game's theme.
-// 3. **HARD MODE:** Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
-// 4. Save your whole game and its properties in an object.
-// 5. Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
-// 6. Don't forget to place your global variables and functions above your object.
-//    * Remember: global variables, then objects, then calls.
-// 7. Definitely talk with a TA or your instructor if you get tripped up during this challenge.
     var listOfWords = [ 
     "FlashMan", 
     "ShadowMan", 
@@ -42,31 +22,6 @@ $(document).ready(function() {
 
     resetGame()
 
-    // var word = listOfWords[Math.floor(Math.random()* words.length)].toUpperCase()
-    // console.log(wordToMatch)
-    // document.write(myGame[random]); 
-   
-    //commented out because of order, duplicates, and not clear writing.
-    // var answerArray = [];
-    
-    // }
-    // var remainingLetters = word.length;
-
-    // while (remianingLetters > 0) {
-        //gamecode goes here
-        
-        //when out of guesses we leave this loop
-
-    // const maxGuess = 10
-    // var pauseGame = false
-    // var guessedLetter = []
-    // var guessingWord = []
-    // var wordToMatch
-    // var numGuess
-    // var wins = 0
-
-    // resetGame()
-
 
 
 
@@ -77,7 +32,7 @@ $(document).ready(function() {
             checkForLetter(event.key.toUpperCase())
         }
     }
-//change all to caps to minimize issues, saw example in another build, where lower case and upper wouldn't work
+    //change all to caps
 
     // Check if letter play back a sound, when win what next? 1/13
     function checkForLetter(letter) {
@@ -99,13 +54,13 @@ $(document).ready(function() {
                 if (guessingWord.join("") === wordToMatch) {
                     wins++
                     pauseGame === false
-                    console.log(wins)     //check this again looking for how to stop constant win totals (4x- registering other in string as wins)
+                    console.log(wins)     //testing this, increments per guess
                     updateDisplay()
                     setTimeout(resetGame,1000)
                 }
             }      
                 
-        // use an array to put string together to see word
+        // using an array to put string together to see word
             
         if (!foundLetter) {
            incorrectSound.play()
@@ -115,13 +70,12 @@ $(document).ready(function() {
             }
             
             if (numGuess === 0) {
-        //         // Displays word before reset
+               // Displays word before reset
                 guessingWord = wordToMatch.split()
-                pauseGame = false   //why would we?
+                pauseGame = false   
                 setTimeout(resetGame,1000)
             }
         }
-//these var's are so similiar in name this is a nightmare to sort out, I might need to add a third word onto some of them to keep it straight in my head
         updateDisplay()
 
     }
@@ -166,9 +120,9 @@ $(document).ready(function() {
         document.getElementById("guessedLetter").innerText =  guessedLetter.join(" ")
     }
 
-    function myFunction() {
-        var x = document.getElementById("myAudio").duration;
-        document.getElementById("liveatUarts2014Betatestmusic").innerHTML = x;
-      }
-      return myFunction;
+    // function myFunction() {
+    //     var x = document.getElementById("myAudio").duration;
+    //     document.getElementById("liveatUarts2014Betatestmusic").innerHTML = x;
+    //   }
+    //   return myFunction;
 })
